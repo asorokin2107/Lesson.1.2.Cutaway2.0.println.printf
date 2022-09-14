e615f57b970595635671d13a62b542fb6167f2ae
Anatoly Sorokin <113344170+asorokin2107@users.noreply.github.com>
2022-09-14 11:08:02 +0300

    Add files via upload

---
+++ Lesson 1.4.1 Cutaway println+f+variable/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Анатолий";
+        String skype = "live:.cid.19ff25dea2ad7ef";
+
+
+        System.out.println("***********************************");
+        System.out.println("Визитка");
+        System.out.printf("Name: %s \nSkype: %s \n", name, skype);
+        System.out.println("***********************************");
+
+    }
+}
